c49f354921d8bd1830950c41fde180be04cad51a
ritabc <11031915+ritabc@users.noreply.github.com>
2024-02-05 12:42:02 -0500

    clox: rework compiler for emitting bytecode, write code generation util funcs

--- clox/compiler.c
+++ clox/compiler.c
@@ -3,26 +3,102 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "compiler.h"
 #include "common.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    initScanner(source);
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError; // did any errors occur during compilation?
 
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            print("   | ");
-        }
-        // print token type and then the 1st token.length chars of token.start
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+    // if in panicMode, suppress any other errors that get detected, but keep compiling
+    // panic mode ends when parser reaches a synchronization point (a statement boundary in Lox)
+    bool panicMode;
+} Parser;
 
-        if (token.type == TOKEN_EOF) break;
+Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // Nothing.
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char* message) {
+    errorAt(&parser.previous, message)
+}
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
+    // keep looping and reporting errors
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+// advances, but also validates whether the token has an expected type
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk, byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
+bool compile(const char*source, Chunk* chunk) {
+    initScanner(source);
+    compilingChunk = chunk;
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+    return !parser.hadError;
 }
