b0a4b1ebc4a28b83c69e81f791018a705fc055d0
ritabc <11031915+ritabc@users.noreply.github.com>
2024-02-07 10:23:07 -0500

    clox: Add support for comparison & equality operators

--- clox/vm.c
+++ clox/vm.c
@@ -23,7 +23,7 @@ static void resetStack() {
 static void runtimeError(const char* format, ...) {
     va_list args;
     va_start(args, format);
-    vprintf(stderr, format, args);
+    vfprintf(stderr, format, args);
     va_end(args);
     fputs("\n", stderr);
 
@@ -102,6 +102,14 @@ for (;;) {
             case OP_NIL:        push(NIL_VAL); break;
             case OP_TRUE:       push(BOOL_VAL(true)); break;
             case OP_FALSE:      push(BOOL_VAL(false)); break;
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(valuesEqual(a, b)));
+                break;
+            }
+            case OP_GREATER:    BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS:       BINARY_OP(BOOL_VAL, <); break;
             case OP_ADD:        BINARY_OP(NUMBER_VAL, +); break;
             case OP_SUBTRACT:   BINARY_OP(NUMBER_VAL, -); break;
             case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
